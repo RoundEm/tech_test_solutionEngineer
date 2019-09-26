2ccc720f11f934c3d7622afd709089c225dadbd4
JASON ROUNDTREE <jasontree123@gmail.com>
2019-09-26 01:23:32 -0700

    Change dynamic html to be rendered by template literals

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+// Notes:
+// - I'm not a big fan of semi-colons if they're not totally necessary so I usually leave them out in my JS but I'd of course use them if the company or client requires them
+// - For some reason the getIpInfo function, which determines what language the header is, works locally but not on jsbin
+// - I'm targeting all elements by class since it looks like there could be multiple widgets on one page
+
 const url = 'https://api.taboola.com/1.2/json/apitestaccount/recommendations.get?app.type=web&app.apikey=7be65fc78e52c11727793f68b06d782cff9ede3c&source.id=%2Fdigiday-publishing-summit%2F&source.url=https%3A%2F%2Fblog.taboola.com%2Fdigiday-publishing-summit%2F&source.type=text&placement.organic-type=mix&%20placement.visible=true&placement.available=true&placement.rec-count=6&%20placement.name=Below%20Article%20Thumbnails&%20placement.thumbnail.width=640&placement.thumbnail.height=480&user.session=init'
 
 async function getWidgetData() {
-    const data = await (await fetch(url)).json()
+    let data = await (await fetch(url)).json()
     return data
 }
 
 async function getIpInfo() {
-    const data = await (await fetch('http://ip-api.com/json')).json()
+    let data = await (await fetch('https://ip-api.com/json')).json()
+    console.log('getIpInfo data: ', data)
     return data
 }
 
@@ -14,49 +20,51 @@ getWidgetData()
     .then(data => {
         renderAds(data)
     })
-    .catch(error => console.log('error: ', error))
+    .catch(error => console.log(error))
 
 function renderAds(data) {
     const adsContainer = document.getElementsByClassName('ads_container')[0]
-  
+    
     data.list.forEach((item, i) => {
         let itemContainer = document.createElement('div')
-        let destinationUrl = `${item.url}`
-        let itemTitle = `${item.name}`
+        let title = item.name
+        let category = item.categories[0]
+        let destinationUrl = item.url
 
-        let img = document.createElement('img')
-        img.src = `${item.thumbnail[0].url}`
-        img.height = "300"
-        img.alt = `Image for ${item.categories[0] ? item.categories[0] : 'an'} advertisement`
-        let hrefImg = document.createElement('a')
-        hrefImg.title = itemTitle
-        hrefImg.target = "_blank"
-        hrefImg.href = destinationUrl
-        hrefImg.appendChild(img)
+        // TODO: Figure out how to make this DRY so the <a> code isn't being repeated
+        const itemHtml = `
+            <a 
+                href=${destinationUrl}
+                title=${title}
+                target="_blank"
+            >
+                <img 
+                    height="300"
+                    alt="Image for ${category ? category : 'an'} advertisement"
+                    src=${item.thumbnail[0].url}
+                />
+            </a>
 
-        let titleSpan = document.createElement('span')
-        titleSpan.classList.add('title', 'ellipses')
-        let title = document.createTextNode(`${item.name}`)
-        titleSpan.appendChild(title)
-        let hrefTitle = document.createElement('a')
-        hrefTitle.title = itemTitle
-        hrefTitle.target = "_blank"
-        hrefTitle.href = destinationUrl
-        hrefTitle.appendChild(titleSpan)
-        
-        let brandSpan = document.createElement('span')
-        brandSpan.classList.add('branding')
-        let brand = document.createTextNode(`${item.branding}`)
-        brandSpan.appendChild(brand)
-        let hrefBrand = document.createElement('a')
-        hrefBrand.title = itemTitle
-        hrefBrand.target = "_blank"
-        hrefBrand.href = destinationUrl
-        hrefBrand.appendChild(brandSpan)
-        
-        itemContainer.appendChild(hrefImg)
-        itemContainer.appendChild(hrefTitle)
-        itemContainer.appendChild(hrefBrand)
+            <a 
+                href=${destinationUrl}
+                title=${title}
+                target="_blank"
+            >
+                <span class="title ellipses">${title}</branding>
+            </a>
+
+            <a 
+                href=${destinationUrl}
+                title=${title}
+                target="_blank"
+            >
+                <span class="branding">
+                    ${item.branding} ${category ? `| (${category})` : ''}
+                </span>
+            </a>
+
+        `
+        itemContainer.innerHTML += itemHtml
         
         adsContainer.appendChild(itemContainer)
     })
@@ -64,16 +72,18 @@ function renderAds(data) {
 
 getIpInfo()
     .then(data => {
+        console.log('ip data: ', data)
         formatHeaderLang(data)
     })
-    .catch(error => console.log('error: ', error))
+    .catch(error => console.log(error))
 
 function formatHeaderLang(ipInfo) {
     const { country } = ipInfo
-    const header = document.querySelector('span.widget_header')
+    const header = document.getElementsByClassName('header_span')[0]
     if (country.toLowerCase() !== 'united states') {
         header.innerHTML = 'Tu Peux Aimer'
     } else {
-        header.innerHTML = 'You May Like'
+        header.innerHTML = 'Around the Web'
     }
 }
+
