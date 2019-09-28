91225c397f36a4fdac31bfb90c1e33ce066ac3a7
JASON ROUNDTREE <jasontree123@gmail.com>
2019-09-27 18:37:17 -0700

    Actual submitted version

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Notes:
 // - I'm not a big fan of semi-colons if they're not totally necessary so I've omitted them but I'd of course use them if the company or client requires them
-// - For some reason the getIpInfo function, which determines what language the header is, works locally but has been kind of squirrely in jsbin for some reason. Hopefully it behaves
+// - This getIpInfo function, which determines what language the header is, works locally but not on jsbin. I think it may have something to do with the api endpoint not being https
 // - I'm targeting all elements by class since it looks like there could be multiple widgets on one page
 
 const url = 'https://api.taboola.com/1.2/json/apitestaccount/recommendations.get?app.type=web&app.apikey=7be65fc78e52c11727793f68b06d782cff9ede3c&source.id=%2Fdigiday-publishing-summit%2F&source.url=https%3A%2F%2Fblog.taboola.com%2Fdigiday-publishing-summit%2F&source.type=text&placement.organic-type=mix&%20placement.visible=true&placement.available=true&placement.rec-count=6&%20placement.name=Below%20Article%20Thumbnails&%20placement.thumbnail.width=640&placement.thumbnail.height=480&user.session=init'
